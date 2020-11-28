e1fa0fffbb1bbb24b4f6a2a19b4d95db2e1be006
Blaze404 <mus.qazi999@gmail.com>
2020-11-29 00:20:07 +0530

    initial release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/blaze4o4/dbaicdten",
-    download_url="https://github.com/blaze4o4/dbaicdten/archive/0.1.tar.gz",
+    download_url="https://github.com/blaze4o4/dbaicdten/archive/0.0.1.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
